ec32ed0fbb2b7084834403450a804c7237a4051b
afrontend <autoscripts@gmail.com>
2020-02-24 23:18:51 +0900

    Add buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <v-button>이거요</v-button>
+    <div>
+      <v-row>
+        <v-col cols="8" sm="6" md="3">
+          <v-text-field label="Menu Name" />
+        </v-col>
+        <v-col cols="4" sm="6" md="3">
+          <v-btn>이거요</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-for="i in 4" :key="i">
+      <v-btn-toggle v-model="toggle_one">
+        <v-btn>아아 x 10 </v-btn>
+        <v-btn> X </v-btn>
+      </v-btn-toggle>
+    </div>
   </div>
 </template>
 
